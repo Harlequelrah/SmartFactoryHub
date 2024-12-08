f2361d8b9ce181fb6dc55c4346d307f836277665
jordan Tomegah <tomegahjordan81@gmail.com>
2024-12-08 20:16:29 +0000

    affichage des données des 7 capteurs

--- auth_app/views.py
+++ auth_app/views.py
@@ -25,7 +25,7 @@ def connexion(request):
         user = authenticate(request, username=username , password= password)
         if user is not None:
             login(request, user)
-            return redirect('accueil')
+            return redirect('sensor:sensor_dashboard')
         else:
             messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
     return  render(request, 'connexion.html')      
